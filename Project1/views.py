37ac4c19f505df280425ed29470dc6f6b8828ad6
OscarAlex <43146741+OscarAlex@users.noreply.github.com>
2021-09-14 19:43:58 -0500

    Templates

---
+++ Project1/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+#Each function is a view
+#Request as argument
+def welcome(request):
+    #Doc path
+    ext_doc= open('C:/Users/Oscar/Desktop/FirstDjango/Project1/Project1/templates/welcome.html')
+    #Read template
+    temp= Template(ext_doc.read())
+    ext_doc.close()
+
+    #Context
+    context= Context()
+
+    doc= temp.render(context)
+    return HttpResponse(doc)
+
+def farewell(request):
+    return HttpResponse("Goodbye")
+
+def getDate(request):
+    actual_date= datetime.datetime.now()
+    return HttpResponse(actual_date)
+
+def calculateAge(request, age, year):
+    period= year-2021
+    future_age= age+period
+    return HttpResponse("On the year {0} you will be {1} years old".format(year, future_age))
